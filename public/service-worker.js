40dfc7337c5ce8a80559c928e60fea5f4d2b3d0f
John Garner <jdextergarneriv@gmail.com>
2021-01-21 18:20:38 -0500

    tweak service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -42,22 +42,61 @@ self.addEventListener("activate", (event) => {
 	);
 });
 
-self.addEventListener("fetch", (event) => {
-	if (event.request.url.startsWith(self.location.origin)) {
-		event.respondWith(
-			caches.match(event.request).then((cachedResponse) => {
-				if (cachedResponse) {
-					return cachedResponse;
-				}
+self.addEventListener("fetch", function (evt) {
+	// cache successful requests to the API
+	if (evt.request.url.includes("/api/")) {
+		evt.respondWith(
+			caches
+				.open(RUNTIME)
+				.then((cache) => {
+					return fetch(evt.request)
+						.then((response) => {
+							// If the response was good, clone it and store it in the cache.
+							if (response.status === 200) {
+								cache.put(evt.request.url, response.clone());
+							}
 
-				return caches.open(RUNTIME).then((cache) => {
-					return fetch(event.request).then((response) => {
-						return cache.put(event.request, response.clone()).then(() => {
 							return response;
+						})
+						.catch((err) => {
+							// Network request failed, try to get it from the cache.
+							return cache.match(evt.request);
 						});
-					});
-				});
-			})
+				})
+				.catch((err) => console.log(err))
 		);
+
+		return;
 	}
+
+	// if the request is not for the API, serve static assets using "offline-first" approach.
+	// see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+	evt.respondWith(
+		caches.match(evt.request).then(function (response) {
+			return response || fetch(evt.request);
+		})
+	);
 });
+
+// self.addEventListener("fetch", (event) => {
+// 	if (event.request.url.startsWith(self.location.origin)) {
+// 		event.respondWith(
+// 			caches
+// 				.match(event.request)
+// 				.then((cachedResponse) => {
+// 					if (cachedResponse) {
+// 						return cachedResponse;
+// 					}
+
+// 					return caches.open(RUNTIME).then((cache) => {
+// 						return fetch(event.request).then((response) => {
+// 							return cache.put(event.request, response.clone()).then(() => {
+// 								return response;
+// 							});
+// 						});
+// 					});
+// 				})
+// 				.catch((err) => console.log(err))
+// 		);
+// 	}
+// });
